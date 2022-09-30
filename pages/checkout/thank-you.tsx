73e2d7926d2089e2cefa8adaac8dedcb09cd344a
Srikanth <srikanth.r@cnetric.com>
2022-09-30 18:18:37 +0530

    uncomments thank you page

--- pages/checkout/thank-you.tsx
+++ pages/checkout/thank-you.tsx
@@ -203,9 +203,11 @@ const ThankYou: NextPage = () => {
                   ` ` +
                   deliveryDate.getFullYear()}
               </li>
-              <br />
+              <li className="my-5">
+                <img width={150} src="/images/thank-you.gif" alt="" />
+              </li>
               <li className="list-inline-item">
-                {/* <a href="/" className="btn fs-18 w-100" tabIndex={0}>
+                <a href="/" className="btn fs-18 w-100" tabIndex={0}>
                   Track Order
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -218,28 +220,6 @@ const ThankYou: NextPage = () => {
                     <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
                     <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
                   </svg>
-                </a> */}
-                <a
-                  onClick={() => {
-                    router.replace(Permalink.ofHomePage());
-                  }}
-                  className=" fs-16 b-t-h btn  border font-sb  text-center w-30 mt-4"
-                  style={{ color: "white" }}
-                >
-                  Back to home page{" "}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={16}
-                    height={16}
-                    fill="currentColor"
-                    className="bi bi-arrow-up-right"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"
-                    />
-                  </svg>
                 </a>
               </li>
               <li className="list-inline-item">
@@ -247,7 +227,7 @@ const ThankYou: NextPage = () => {
                   onClick={() => {
                     setInvoiceStatus(true);
                   }}
-                  className="btn fs-18 w-100 btn-default mt-4"
+                  className="btn fs-18 w-100 btn-default"
                   tabIndex={0}
                 >
                   Invoice
@@ -264,19 +244,6 @@ const ThankYou: NextPage = () => {
                 </a>
               </li>
             </ul>
-
-            <div>
-              {/* <ReactToPdf targetRef={ref} filename="code-example.pdf" options={options}>
-        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-      </ReactToPdf>
-      <div ref={ref}>
-          <OrderInvoice/>
-        </div> */}
-
-              {/* <PDFDownloadLink document={<OrderInvoice />} fileName="invoice.pdf">
-      {({ blob, url, loading, error }) => (loading ? 'Loading document...' : 'Download now!')}
-    </PDFDownloadLink> */}
-            </div>
           </div>
         </div>
         {loading ? (
@@ -298,14 +265,14 @@ const ThankYou: NextPage = () => {
           orderItems={orderItems}
         />
       </section>
-      {/* <OrderInvoice /> */}
-      {/* <section className="mt-5">
+
+      <section className="mt-5">
         <a href="#">
           <img className="w-100" src="images/advertise.png" alt="" />
         </a>
-      </section> */}
+      </section>
 
-      {/* <VisitNunchiBanner /> */}
+      <VisitNunchiBanner />
     </div>
   );
 };
