3f03968b90151c61382e85085fca5a6baeb01b1f
Tejpal_Singh <tejpal@cnetric.com>
2022-09-30 15:54:08 +0530

    landing & pdp uncommented

--- app/themes/themeOne/components/FooterFixedButton.tsx
+++ app/themes/themeOne/components/FooterFixedButton.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 const FooterFixedButton = () => {
     return (
-        <div className="fixbtn">
-            <a href="#" className="scrollToTop">
-                <i className="fas fa-angles-up" />
-            </a>
-            {/* <a href="#" className="whatsapp">
-                <i className="fa-brands fa-whatsapp" />
-            </a> */}
-        </div>
+        <>
+            <div className="fixbtn">
+                <a href="#" className="scrollToTop">
+                    <i className="fas fa-angles-up" />
+                </a>
+                <a target={"_blank"} href={`https://api.whatsapp.com/send?phone=+91987654321&text=Hi, I'm a visitor of NavTatva`} className="whatsapp">
+                    <i className="fa-brands fa-whatsapp" />
+                </a>
+            </div>
+        </>
     )
 }
 
